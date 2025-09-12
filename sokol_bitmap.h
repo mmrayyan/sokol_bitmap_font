da1569273b939af268af40b80beca84a7b34956d
Muhammad Mohsen <amily.m7md@gmail.com>
2025-09-12 18:04:17 +0300

    Add JSDoc comments

--- sokol_bitmap.h
+++ sokol_bitmap.h
@@ -5,19 +5,20 @@
 #include "sokol_gp.h"
 
 typedef struct sbm_desc {
-  sg_image img;
+  sg_image img;                   // Sokol GFX image
 
-  uint32_t img_width_pixels;
-  uint32_t img_height_pixels;
-  uint32_t img_padding_x_pixels;
-  uint32_t img_padding_y_pixels;
-  uint32_t char_width_pixels;
-  uint32_t char_height_pixels;
-  uint32_t char_padding_x_pixels;
-  uint32_t char_padding_y_pixels;
+  uint32_t img_width_pixels;      // Image total width
+  uint32_t img_height_pixels;     // Image total height
+  uint32_t img_padding_x_pixels;  // Image outer padding left
+  uint32_t img_padding_y_pixels;  // Image outer padding top
 
-  const char *chars;
-  size_t num_chars;
+  uint32_t char_width_pixels;     // Character width
+  uint32_t char_height_pixels;    // Character height
+  uint32_t char_padding_x_pixels; // Character padding right
+  uint32_t char_padding_y_pixels; // Character padding bottom
+
+  const char *chars;              // Characters in font image
+  size_t num_chars;               // Number of characters
 } sbm_desc;
 
 typedef struct sbm_font {
@@ -31,20 +32,43 @@ typedef struct sbm_font {
 } sbm_font;
 
 typedef struct sbm_draw_opts {
-  const char *string;
-  size_t string_len;
+  const char *string;             // String to draw
+  size_t string_len;              // Number of characters in string
 
-  uint32_t font_size;
+  uint32_t font_size;             // Font size
 
-  float x;
-  float y;
-  float gap;
+  float x;                        // X coordinate (to top left of string)
+  float y;                        // Y coordinate (to top left of string)
+  float gap;                      // Character gap
 } sbm_draw_opts;
 
+/**
+ * Initializes a bitmap font with the given description.
+ * @param self Pointer to the font object to initialize.
+ * @param desc Font description containing image and character info.
+ * @returns true on success, false on failure.
+ */
 bool sbm_init_font(sbm_font *self, sbm_desc desc);
+
+/**
+ * Frees resources associated with a bitmap font.
+ * @param self Pointer to the font object to free.
+ */
 void sbm_free_font(sbm_font *self);
 
+/**
+ * Draws a single character using the bitmap font.
+ * @param self Pointer to the font object.
+ * @param c Character to draw.
+ * @param r Rectangle specifying position and size.
+ */
 void sbm_draw_char(sbm_font *self, char c, sgp_rect r);
+
+/**
+ * Draws a string using the bitmap font and drawing options.
+ * @param self Pointer to the font object.
+ * @param opts Drawing options including string, position, and font size.
+ */
 void sbm_draw_string(sbm_font *self, sbm_draw_opts opts);
 
 #endif
